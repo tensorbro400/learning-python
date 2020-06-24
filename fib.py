10cf231792821c71bc8f0aab207c12adffd5f0c9
tensorbro400 <67087673+tensorbro400@users.noreply.github.com>
2020-06-24 01:56:35 -0700

    fib.py 1.2
    Allows user control of max value.

--- fib.py
+++ fib.py
@@ -1,7 +1,8 @@
 prev_num = 0
 num = 1
+limit = input("enter max value: ")
 print(num)
-while num + prev_num <= 1000:
+while num + prev_num <= limit:
     num += prev_num
     print(num)
     prev_num = num - prev_num
